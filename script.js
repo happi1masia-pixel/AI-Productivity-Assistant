64527f049352909ad956968e651168f435c1ee83
happi1masia-pixel <happi1masia@gmail.com>
2026-07-15 14:07:19 +0200

    Add click event to buttons for AI response alert

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        alert("This feature will use AI to generate a response.");
+    });
+});
